c2bb5722f4f71883c7337e79e2c43311408f0471
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-10 10:19:14 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
 basic.forever(function () {
-	
+    if (input.lightLevel() > 50) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
